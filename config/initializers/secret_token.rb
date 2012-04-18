4d441aefed126a77d73240102e855166eb5834ed
Erin Walker <ewalker@ewalkerblog.co.za>
2012-04-18 22:06:20 +0200

    initial_commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Storky::Application.config.secret_token = '19bb8d3ccf1dc55c005d195a6625fddf9574c3126f927379ead8d113d94e4b7a5814e28c7b7d351f2848d8832d447dc1831e87aaf72f606bd4825c0af54aa536'
